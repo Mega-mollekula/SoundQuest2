7dd117bd580133de09d6d0f7b332c67208e8ec9a
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-01-31 17:28:11 +0300

    feat: interesting facts for loading screen

---
+++ app/src/main/java/com/example/soundquest2/ui/facts/FactsCatalog.kt
@@ -0,0 +1,29 @@
+package com.example.soundquest2.ui.facts
+
+import com.example.soundquest2.R
+
+object FactsCatalog {
+
+    val facts = listOf(
+        InterestingFact(
+            imageRes = R.drawable.fact_1,
+            titleRes = R.string.fact_1_title,
+            descriptionRes = R.string.fact_1_desc
+        ),
+        InterestingFact(
+            imageRes = R.drawable.fact_2,
+            titleRes = R.string.fact_2_title,
+            descriptionRes = R.string.fact_2_desc
+        ),
+        InterestingFact(
+            imageRes = R.drawable.fact_3,
+            titleRes = R.string.fact_3_title,
+            descriptionRes = R.string.fact_3_desc
+        ),
+        InterestingFact(
+            imageRes = R.drawable.fact_4,
+            titleRes = R.string.fact_4_title,
+            descriptionRes = R.string.fact_4_desc
+        )
+    )
+}
---
+++ app/src/main/java/com/example/soundquest2/ui/facts/InterestingFact.kt
@@ -0,0 +1,7 @@
+package com.example.soundquest2.ui.facts
+
+data class InterestingFact(
+    val imageRes: Int,
+    val titleRes: Int,
+    val descriptionRes: Int
+)
