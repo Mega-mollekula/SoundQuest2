e9f6f0b5fa91d6ee2023623dd3990840f4bdf546
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-01 10:35:15 +0300

    refactor: changes in main activity for full screen mode

--- app/src/main/java/com/example/soundquest2/MainActivity.kt
+++ app/src/main/java/com/example/soundquest2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.remember
+import androidx.core.view.WindowCompat
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
@@ -18,6 +19,9 @@ import com.example.soundquest2.data.local.storage.ThemeStorage
 import com.example.soundquest2.ui.theme.SoundQuestThemeProvider
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 val Context.themeDataStore by preferencesDataStore(name = "theme_store")
 val Context.languageDataStore: DataStore<Preferences> by preferencesDataStore(name = "language_store")
@@ -53,11 +57,15 @@ class MainActivity : ComponentActivity() {
             controller.getCurrentTheme()
         }
 
+        enableEdgeToEdge()
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableImmersiveMode()
+
         setContent {
             val properties = remember(theme) {
                 resolver.resolve(theme)
             }
-
             SoundQuestThemeProvider(properties) {
                 //AppNavGraph()
             }
@@ -76,5 +84,15 @@ class MainActivity : ComponentActivity() {
             languageStorage.setLanguage(languageCode)
             recreate()
         }
+    }
+}
+
+fun ComponentActivity.enableImmersiveMode() {
+    WindowInsetsControllerCompat(window, window.decorView).apply {
+        hide(
+            WindowInsetsCompat.Type.statusBars() or
+                    WindowInsetsCompat.Type.navigationBars()
+        )
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
