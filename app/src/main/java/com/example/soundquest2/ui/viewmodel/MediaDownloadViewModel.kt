f2a090ac427af6cad4294b1ef35a9682e5e1dd77
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-03 01:34:46 +0300

    feat: vm for downloading

---
+++ app/src/main/java/com/example/soundquest2/ui/viewmodel/MediaDownloadViewModel.kt
@@ -0,0 +1,87 @@
+package com.example.soundquest2.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.soundquest2.core.errors.AppError
+import com.example.soundquest2.core.errors.Result
+import com.example.soundquest2.data.local.download.DownloadProgress
+import com.example.soundquest2.domain.model.GameMode
+import com.example.soundquest2.domain.usecase.DownloadMediaUseCase
+import com.example.soundquest2.domain.usecase.LoadMediaUseCase
+import com.example.soundquest2.ui.state.DownloadUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class MediaDownloadViewModel(
+    private val loadAudio: LoadMediaUseCase,
+    private val downloadMedia: DownloadMediaUseCase,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<DownloadUiState>(DownloadUiState.Idle)
+    val uiState: StateFlow<DownloadUiState> = _uiState.asStateFlow()
+
+    fun startDownload(gameMode: GameMode, language: String, count: Int) {
+
+        if (_uiState.value is DownloadUiState.Downloading) return
+
+        viewModelScope.launch {
+            //getting metadata before downloading
+            when(val result = loadAudio(forceRefresh = true, gameMode, language, count)) {
+
+                is Result.Success -> {
+                    downloadMedia().collect { result ->
+                        when (result) {
+
+                            is Result.Success -> {
+                                handleProgress(result.data)
+                            }
+
+                            is Result.Error -> {
+                                _uiState.value = DownloadUiState.Error(result.error)
+                            }
+                        }
+                    }
+                }
+                is Result.Error -> {
+                    _uiState.update {
+                        DownloadUiState.Error(
+                            error = result.error
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleProgress(progress: DownloadProgress) {
+        when (progress) {
+
+            is DownloadProgress.InProgress -> {
+                _uiState.value = DownloadUiState.Downloading(
+                    completed = progress.completed,
+                    total = progress.total,
+                    failed = progress.failed,
+                    skipped = progress.skipped
+                )
+            }
+
+            is DownloadProgress.Completed -> {
+                if (progress.total > 0 && progress.completed == 0) {
+                    _uiState.value = DownloadUiState.Error(
+                        AppError.NoContent("Не удалось загрузить ни одного медиафайла")
+                    )
+                } else {
+                    _uiState.value = DownloadUiState.Completed(
+                        completed = progress.completed,
+                        total = progress.total,
+                        failed = progress.failed,
+                        skipped = progress.skipped
+                    )
+                }
+            }
+        }
+    }
+}
