37f49d931d74d96ae22acabf8576a3cab184d127
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-03 01:12:22 +0300

    refactor: added parametr skipped for DownloadProgress classes

--- app/src/main/java/com/example/soundquest2/data/local/download/DownloadProgress.kt
+++ app/src/main/java/com/example/soundquest2/data/local/download/DownloadProgress.kt
@@ -5,12 +5,14 @@ sealed class DownloadProgress {
     data class InProgress(
         val completed: Int,
         val total: Int,
-        val failed: Int
+        val failed: Int,
+        val skipped: Int
     ) : DownloadProgress()
 
     data class Completed(
         val completed: Int,
         val failed: Int,
-        val total: Int
+        val total: Int,
+        val skipped: Int
     ) : DownloadProgress()
 }
