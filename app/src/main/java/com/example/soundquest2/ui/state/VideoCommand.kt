20dc772b19e9ec026461dc6bbe32c89570832040
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-03 01:24:39 +0300

    fix: deleted files linked with game view model

--- app/src/main/java/com/example/soundquest2/ui/state/VideoCommand.kt
+++
@@ -1,7 +0,0 @@
-package com.example.soundquest2.ui.state
-
-sealed interface VideoCommand {
-    data class Prepare(val paths: List<String>) : VideoCommand
-    data class Play(val index: Int) : VideoCommand
-    object Stop : VideoCommand
-}
