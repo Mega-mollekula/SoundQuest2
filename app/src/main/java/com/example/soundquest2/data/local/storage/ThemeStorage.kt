c738309bc3aef231115b41b0e18f9219318afdac
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-03 17:13:10 +0300

    Revert "refactor: theme now - flow"
    This reverts commit 5a7e44ccadf75295835ff2920903ee92c79fa4c1.

--- app/src/main/java/com/example/soundquest2/data/local/storage/ThemeStorage.kt
+++ app/src/main/java/com/example/soundquest2/data/local/storage/ThemeStorage.kt
@@ -5,9 +5,7 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.example.soundquest2.core.theme.AppTheme
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 
 class ThemeStorage(
     private val dataStore: DataStore<Preferences>
@@ -18,18 +16,17 @@ class ThemeStorage(
         const val DEFAULT_THEME = "LIGHT"
     }
 
-    val themeFlow: Flow<AppTheme> = dataStore.data.map { prefs ->
-        runCatching {
+    suspend fun setTheme(theme: AppTheme) {
+        dataStore.edit {
+            it[THEME] = theme.name
+        }
+    }
+
+    suspend fun getTheme(): AppTheme {
+        return runCatching {
             AppTheme.valueOf(
-                prefs[THEME] ?: DEFAULT_THEME
+                dataStore.data.first()[THEME] ?: DEFAULT_THEME
             )
         }.getOrElse { AppTheme.LIGHT }
     }
-
-    suspend fun setTheme(theme: AppTheme) {
-        dataStore.edit { it[THEME] = theme.name }
-    }
-
-    suspend fun getTheme(): AppTheme =
-        themeFlow.first()
 }
