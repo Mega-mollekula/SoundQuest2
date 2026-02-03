298d22b6b629985be0ee60ddb3860f98751faf0e
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-03 17:13:58 +0300

    Revert "refactor: language now - flow"
    This reverts commit 23df7bc6a9e100ad956fb8842d138a94cc2b77a1.

--- app/src/main/java/com/example/soundquest2/data/local/storage/LanguageStorage.kt
+++ app/src/main/java/com/example/soundquest2/data/local/storage/LanguageStorage.kt
@@ -4,10 +4,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import com.example.soundquest2.core.language.AppLanguage
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 
 class LanguageStorage(
     private val dataStore: DataStore<Preferences>
@@ -18,19 +15,13 @@ class LanguageStorage(
         const val DEFAULT_LANGUAGE = "ru"
     }
 
-    val languageFlow: Flow<AppLanguage> = dataStore.data.map { prefs ->
-        AppLanguage.fromCode(
-            prefs[LANGUAGE] ?: DEFAULT_LANGUAGE
-        )
+    suspend fun setLanguage(code: String) {
+        dataStore.edit {
+            it[LANGUAGE] = code
+        }
     }
 
-    suspend fun setLanguage(language: AppLanguage) {
-        dataStore.edit { it[LANGUAGE] = language.code }
-    }
-
-    suspend fun getLanguage(): AppLanguage {
-        return AppLanguage.fromCode(
-            dataStore.data.first()[LANGUAGE] ?: DEFAULT_LANGUAGE
-        )
+    suspend fun getLanguage(): String {
+        return dataStore.data.first()[LANGUAGE] ?: DEFAULT_LANGUAGE
     }
 }
