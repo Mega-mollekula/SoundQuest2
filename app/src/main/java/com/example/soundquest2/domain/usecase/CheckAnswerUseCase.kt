ec4170c0d783fc0213e3a0e0604620c7f6def623
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-04 16:29:47 +0300

    feat: rework of all useCases: now they work with state and update it

--- app/src/main/java/com/example/soundquest2/domain/usecase/CheckAnswerUseCase.kt
+++ app/src/main/java/com/example/soundquest2/domain/usecase/CheckAnswerUseCase.kt
@@ -1,10 +1,14 @@
 package com.example.soundquest2.domain.usecase
 
-import com.example.soundquest2.domain.model.Round
+import com.example.soundquest2.domain.model.GameState
 import com.example.soundquest2.domain.model.content.MediaContent
 
 class CheckAnswerUseCase {
-    operator fun invoke(selected: MediaContent, round: Round): Boolean {
-        return selected == round.correct
+    operator fun invoke(gameState: GameState, selected: MediaContent): GameState {
+
+        val isCorrect = selected == gameState.currentRound!!.correct
+        return gameState.copy(
+            score = if (isCorrect) gameState.score + 1 else gameState.score
+        )
     }
 }
