56a8e06a901de21c9332def1cafda47a083c8a5e
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-05 22:54:56 +0300

    feat: main icon file

---
+++ app/src/main/java/com/example/soundquest2/ui/component/MainIcon.kt
@@ -0,0 +1,65 @@
+package com.example.soundquest2.ui.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.soundquest2.R
+import com.example.soundquest2.ui.theme.AppTheme
+
+
+@Composable
+fun MainIcon(
+    @DrawableRes iconId: Int,
+) {
+    val shape = RoundedCornerShape(10.dp)
+    Box(
+        modifier = Modifier
+            .size(60.dp)
+            .background(
+                color = MaterialTheme.colorScheme.primary.copy(alpha = 0.6f),
+                shape = shape
+            )
+            .border(
+                width = 0.5.dp,
+                color = MaterialTheme.colorScheme.outline, shape = shape
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = iconId),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(34.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainIconPreview() {
+    AppTheme(true) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+                .padding(16.dp)
+        ) {
+            MainIcon(R.drawable.market_icon)
+        }
+    }
+}
