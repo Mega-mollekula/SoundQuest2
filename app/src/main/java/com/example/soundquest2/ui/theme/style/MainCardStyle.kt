2fc2993294f94df540a54a37f5c00c880124edf0
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-05 18:38:08 +0300

    fix: deleted previous design

--- app/src/main/java/com/example/soundquest2/ui/theme/style/MainCardStyle.kt
+++
@@ -1,9 +0,0 @@
-package com.example.soundquest2.ui.theme.style
-
-import androidx.compose.ui.graphics.Color
-
-data class MainCardStyle(
-    val bottomColor: Color,
-    val topColor: Color,
-    val alpha: Float = 1f
-)
--- app/src/main/java/com/example/soundquest2/ui/theme/style/ResultCardStyle.kt
+++
@@ -1,8 +0,0 @@
-package com.example.soundquest2.ui.theme.style
-
-import androidx.compose.ui.graphics.Color
-
-data class ResultCardStyle(
-    val cardColor: Color,
-    var itemColor: Color
-)
