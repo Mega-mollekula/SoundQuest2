1bb3eb7e7717db89713859299ee630e61cb79282
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-05 22:54:12 +0300

    feat: new design start files

---
+++ app/src/main/java/com/example/soundquest2/ui/theme/Images.kt
@@ -0,0 +1,20 @@
+package com.example.soundquest2.ui.theme
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.example.soundquest2.R
+
+data class AppImages(
+    val bg: Int,
+)
+
+val LightImages = AppImages(
+    bg = R.drawable.bg_light,
+)
+
+val DarkImages = AppImages(
+    bg = R.drawable.bg_dark,
+)
+
+val LocalAppImages = staticCompositionLocalOf<AppImages> {
+    error("AppImages not provided")
+}
