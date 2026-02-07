7d7ed3909675706831dab90507a0a5b4e1de11be
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-07 18:50:41 +0300

    update: commented lines due to big changed in project

--- app/src/main/java/com/example/soundquest2/MainActivity.kt
+++ app/src/main/java/com/example/soundquest2/MainActivity.kt
@@ -9,83 +9,69 @@ import androidx.core.view.WindowCompat
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
-import androidx.lifecycle.lifecycleScope
 import com.example.soundquest2.core.language.AppLocaleController
-import com.example.soundquest2.core.theme.AppTheme
-import com.example.soundquest2.core.theme.ThemeController
-import com.example.soundquest2.core.theme.ThemeResolver
 import com.example.soundquest2.data.local.storage.LanguageStorage
 import com.example.soundquest2.data.local.storage.ThemeStorage
-import com.example.soundquest2.ui.theme.SoundQuestThemeProvider
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.soundquest2.core.theme.AppTheme
+import com.example.soundquest2.ui.viewmodel.SettingsViewModel
+import kotlinx.coroutines.launch
 
 val Context.themeDataStore by preferencesDataStore(name = "theme_store")
 val Context.languageDataStore: DataStore<Preferences> by preferencesDataStore(name = "language_store")
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var controller: ThemeController
-    private lateinit var languageStorage: LanguageStorage
-
     override fun attachBaseContext(newBase: Context) {
-        val languageDataStore = newBase.languageDataStore
-        val storage = LanguageStorage(languageDataStore)
-
         val language = runBlocking {
-            storage.getLanguage()
+            LanguageStorage(newBase.languageDataStore).getLanguage()
         }
-
-        val localeController = AppLocaleController(newBase)
-        localeController.applyLanguage(language)
-
+        AppLocaleController(newBase).apply(language)
         super.attachBaseContext(newBase)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
-        val storage = ThemeStorage(themeDataStore)
-        controller = ThemeController(storage)
-        val resolver = ThemeResolver()
-        languageStorage = LanguageStorage(languageDataStore)
-
-        val theme = runBlocking {
-            controller.getCurrentTheme()
-        }
-
-        enableEdgeToEdge()
-
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        enableImmersiveMode()
-
-        setContent {
-            val properties = remember(theme) {
-                resolver.resolve(theme)
-            }
-            SoundQuestThemeProvider(properties) {
-                //AppNavGraph()
-            }
-        }
-    }
-
-    private fun changeTheme(theme: AppTheme) {
         lifecycleScope.launch {
-            controller.changeTheme(theme)
-            recreate()
-        }
-    }
 
-    private fun changeLanguage(languageCode: String) {
-        lifecycleScope.launch {
-            languageStorage.setLanguage(languageCode)
-            recreate()
+            val theme = ThemeStorage(themeDataStore).getTheme()
+            val language = LanguageStorage(languageDataStore).getLanguage()
+
+            val settingsViewModel = SettingsViewModel(
+                ThemeStorage(themeDataStore),
+                LanguageStorage(languageDataStore),
+                theme,
+                language
+            )
+
+            enableFullScreenMode()
+
+            setContent {
+
+//                val theme by settingsViewModel.theme.collectAsState()
+//                val language by settingsViewModel.language.collectAsState()
+//
+//                LaunchedEffect(language) {
+//                    AppLocaleController(this@MainActivity).apply(language)
+//                }
+
+//                SoundQuestThemeProvider(properties) {
+//                    //AppNavGraph()
+//                }
+            }
         }
     }
 }
+
 
 fun ComponentActivity.enableImmersiveMode() {
     WindowInsetsControllerCompat(window, window.decorView).apply {
@@ -95,4 +81,10 @@ fun ComponentActivity.enableImmersiveMode() {
         )
         systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
+}
+
+fun ComponentActivity.enableFullScreenMode() {
+    enableEdgeToEdge()
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    enableImmersiveMode()
 }
