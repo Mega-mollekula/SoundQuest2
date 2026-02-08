6e49ee28158f4185712217e9ad14e1bb7fced0cd
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-08 19:59:15 +0300

    feat: screen for song details

---
+++ app/src/main/java/com/example/soundquest2/ui/screen/main/SongSelectionScreen.kt
@@ -0,0 +1,144 @@
+package com.example.soundquest2.ui.screen.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.soundquest2.R
+import com.example.soundquest2.domain.model.GameMode
+import com.example.soundquest2.domain.model.enums.Era
+import com.example.soundquest2.domain.model.enums.Genre
+import com.example.soundquest2.ui.component.ActionButton
+import com.example.soundquest2.ui.component.DropdownSelector
+import com.example.soundquest2.ui.component.MainBackground
+import com.example.soundquest2.ui.theme.AppTheme
+import com.example.soundquest2.ui.theme.AppTypography
+import com.example.soundquest2.ui.theme.LocalAppImages
+import com.example.soundquest2.ui.util.localizedName
+
+@Composable
+fun SongSelectionScreen(
+    startDownload: (gameMode: GameMode, language: String, count: Int) -> Unit,
+    navigateToDownloadScreen: () -> Unit,
+    onExit: () -> Unit,
+) {
+
+    var selectedEra by remember { mutableStateOf<Era?>(null) }
+    var selectedGenre by remember { mutableStateOf<Genre?>(null) }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        MainBackground(LocalAppImages.current.bg)
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.5f)),
+            contentAlignment = Alignment.Center
+        ) {
+
+            ActionButton(
+                modifier = Modifier.align(Alignment.TopStart).padding(16.dp),
+                text = stringResource(R.string.back),
+                onAction = onExit
+            )
+
+            Column(
+                modifier = Modifier.fillMaxWidth().offset(y = -60.dp),
+                verticalArrangement = Arrangement.spacedBy(30.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.select_parameters),
+                    style = AppTypography.titleMedium,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+
+                DropdownSelector(
+                    items = Era.entries,
+                    selectedItem = selectedEra,
+                    onItemSelected = { era -> selectedEra = era },
+                    itemLocalization = { it.localizedName() },
+                    placeholder = stringResource(R.string.select_decade)
+                )
+
+                DropdownSelector(
+                    items = Genre.entries,
+                    selectedItem = selectedGenre,
+                    onItemSelected = { genre -> selectedGenre = genre },
+                    itemLocalization = { it.localizedName() },
+                    placeholder = stringResource(R.string.select_genre)
+                )
+
+                ActionButton(
+                    text = stringResource(R.string.start),
+                    onAction = {
+                        val gameMode = GameMode.GuessSong(
+                            era = selectedEra,
+                            genre = selectedGenre
+                        )
+                        startDownload(
+                            gameMode,
+                            "en",
+                            10
+                        )
+                        navigateToDownloadScreen()
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    name = "Light",
+    showBackground = true,
+    locale = "ru"
+)
+@Composable
+fun SongSelectionScreenLight() {
+    AppTheme(darkTheme = false) {
+        SongSelectionScreen(
+            startDownload = { _, _, _ -> },
+            navigateToDownloadScreen = {},
+            onExit = {},
+        )
+    }
+}
+
+@Preview(
+    name = "Dark",
+    showBackground = true,
+    locale = "en"
+)
+@Composable
+fun SongSelectionScreenDark() {
+    AppTheme(darkTheme = true) {
+        SongSelectionScreen(
+            startDownload = { _, _, _ -> },
+            navigateToDownloadScreen = {},
+            onExit = {},
+        )
+    }
+}
