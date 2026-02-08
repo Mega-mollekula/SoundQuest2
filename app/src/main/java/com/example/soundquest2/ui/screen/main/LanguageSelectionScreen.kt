a84b05fb4c00e78edeef1f2b4064b96364ffda88
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-08 10:08:25 +0300

    feat: language selection screen

---
+++ app/src/main/java/com/example/soundquest2/ui/screen/main/LanguageSelectionScreen.kt
@@ -0,0 +1,95 @@
+package com.example.soundquest2.ui.screen.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.soundquest2.R
+import com.example.soundquest2.ui.component.MainBackground
+import com.example.soundquest2.ui.component.MainButton
+import com.example.soundquest2.ui.theme.AppTheme
+import com.example.soundquest2.ui.theme.AppTypography
+import com.example.soundquest2.ui.theme.LocalAppImages
+
+@Composable
+fun LanguageSelectionScreen(
+    onRusLanguageClick: () -> Unit,
+    onEnLanguageClick: () -> Unit,
+) {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        MainBackground(LocalAppImages.current.bg)
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.5f)),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Column(
+                modifier = Modifier.fillMaxWidth().offset(y = -60.dp),
+                verticalArrangement = Arrangement.spacedBy(30.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.choose_language),
+                    style = AppTypography.titleLarge,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+
+                MainButton(
+                    text = "Русский",
+                    onClick = onRusLanguageClick
+                )
+
+                MainButton(
+                    text = "English",
+                    onClick = onEnLanguageClick
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    name = "Dark",
+    locale = "ru"
+)
+@Composable
+fun LanguageSelectionScreenLight() {
+    AppTheme(darkTheme = false) {
+        LanguageSelectionScreen(
+            {}, {}
+        )
+    }
+}
+
+@Preview(
+    name = "Dark",
+    locale = "en"
+)
+@Composable
+fun LanguageSelectionScreenDark() {
+    AppTheme(darkTheme = true) {
+        LanguageSelectionScreen(
+            {}, {}
+        )
+    }
+}
