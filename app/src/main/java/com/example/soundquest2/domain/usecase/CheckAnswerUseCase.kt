f46de4d7b726aeb61ed13421403ec601bf3d5c31
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-10 16:25:04 +0300

    fix: change of gamePhase

--- app/src/main/java/com/example/soundquest2/domain/usecase/CheckAnswerUseCase.kt
+++ app/src/main/java/com/example/soundquest2/domain/usecase/CheckAnswerUseCase.kt
@@ -2,12 +2,14 @@ package com.example.soundquest2.domain.usecase
 
 import com.example.soundquest2.domain.model.GameState
 import com.example.soundquest2.domain.model.content.MediaContent
+import com.example.soundquest2.domain.model.enums.GamePhase
 
 class CheckAnswerUseCase {
     operator fun invoke(gameState: GameState, selected: MediaContent): GameState {
 
         val isCorrect = selected == gameState.currentRound!!.correct
         return gameState.copy(
+            gamePhase = GamePhase.RESULT,
             score = if (isCorrect) gameState.score + 1 else gameState.score
         )
     }
