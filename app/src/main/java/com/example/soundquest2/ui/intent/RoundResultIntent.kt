50af622827378c38966e154b3274f6ba1e19d181
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-10 16:36:25 +0300

    update: new intent for launching video

--- app/src/main/java/com/example/soundquest2/ui/intent/RoundResultIntent.kt
+++ app/src/main/java/com/example/soundquest2/ui/intent/RoundResultIntent.kt
@@ -1,6 +1,7 @@
 package com.example.soundquest2.ui.intent
 
 sealed interface RoundResultIntent {
+    data object ScreenShown : RoundResultIntent
     data object ContinueClicked : RoundResultIntent
     data object FavouriteClicked : RoundResultIntent
 }
