04d9dbf277fa3342749d2f21bf4ca7e2fe0bc342
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-11 15:49:56 +0300

    update: checking by id

--- app/src/main/java/com/example/soundquest2/domain/usecase/CheckAnswerUseCase.kt
+++ app/src/main/java/com/example/soundquest2/domain/usecase/CheckAnswerUseCase.kt
@@ -1,13 +1,12 @@
 package com.example.soundquest2.domain.usecase
 
 import com.example.soundquest2.domain.model.GameState
-import com.example.soundquest2.domain.model.content.MediaContent
 import com.example.soundquest2.domain.model.enums.GamePhase
 
 class CheckAnswerUseCase {
-    operator fun invoke(gameState: GameState, selected: MediaContent): GameState {
+    operator fun invoke(gameState: GameState, id: Long): GameState {
 
-        val isCorrect = selected == gameState.currentRound!!.correct
+        val isCorrect = id == gameState.currentRound!!.correct.id
         return gameState.copy(
             gamePhase = GamePhase.RESULT,
             score = if (isCorrect) gameState.score + 1 else gameState.score
