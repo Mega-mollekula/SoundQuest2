730aa9a4e580a5eb1757150d9985d8e60aae0e24
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-13 23:55:15 +0300

    refactor: deleted preview

--- app/src/main/java/com/example/soundquest2/ui/component/MainIcon.kt
+++ app/src/main/java/com/example/soundquest2/ui/component/MainIcon.kt
@@ -6,22 +6,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.soundquest2.R
-import com.example.soundquest2.ui.theme.AppTheme
-
 
 @Composable
 fun MainIcon(
@@ -51,20 +44,5 @@ fun MainIcon(
             contentScale = ContentScale.Fit,
             modifier = Modifier.size(34.dp),
         )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainIconPreview() {
-    AppTheme(true) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black)
-                .padding(16.dp)
-        ) {
-            MainIcon(R.drawable.market_icon, {})
-        }
     }
 }
