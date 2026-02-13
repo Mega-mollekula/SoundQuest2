ee1f3c2a643c8761bd86d2b257b9dd0f012ad9fb
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-14 00:07:49 +0300

    update: implement dependencies without di

--- app/src/main/java/com/example/soundquest2/MainActivity.kt
+++ app/src/main/java/com/example/soundquest2/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.remember
 import androidx.core.view.WindowCompat
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -20,7 +19,36 @@ import androidx.compose.runtime.getValue
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.rememberNavController
+import com.example.soundquest2.core.media.ExoAudioPlayer
+import com.example.soundquest2.core.media.ExoVideoPlayer
+import com.example.soundquest2.core.media.MenuExoPlayer
 import com.example.soundquest2.core.theme.AppTheme
+import com.example.soundquest2.data.AppDatabase
+import com.example.soundquest2.data.local.download.MediaDaos
+import com.example.soundquest2.data.local.download.UnifiedMediaDownloader
+import com.example.soundquest2.data.local.storage.AndroidFileProvider
+import com.example.soundquest2.data.remote.api.ApiService
+import com.example.soundquest2.data.repository.FilmRepositoryImpl
+import com.example.soundquest2.data.repository.GameRepositoryImpl
+import com.example.soundquest2.data.repository.GameResultRepositoryImpl
+import com.example.soundquest2.data.repository.SongRepositoryImpl
+import com.example.soundquest2.domain.usecase.CheckAnswerUseCase
+import com.example.soundquest2.domain.usecase.DownloadMediaUseCase
+import com.example.soundquest2.domain.usecase.FinishGameUseCase
+import com.example.soundquest2.domain.usecase.GenerateRoundsUseCase
+import com.example.soundquest2.domain.usecase.InsertResultUseCase
+import com.example.soundquest2.domain.usecase.LoadMediaUseCase
+import com.example.soundquest2.domain.usecase.NextRoundUseCase
+import com.example.soundquest2.domain.usecase.ResetGameUseCase
+import com.example.soundquest2.domain.usecase.SetGameModeUseCase
+import com.example.soundquest2.domain.usecase.StartGameUseCase
+import com.example.soundquest2.ui.navigator.AppNavHost
+import com.example.soundquest2.ui.playback.MenuMusicController
+import com.example.soundquest2.ui.playback.VideoPreparer
+import com.example.soundquest2.ui.theme.SoundQuestTheme
+import com.example.soundquest2.ui.viewmodel.GameViewModel
+import com.example.soundquest2.ui.viewmodel.MediaDownloadViewModel
 import com.example.soundquest2.ui.viewmodel.SettingsViewModel
 import kotlinx.coroutines.launch
 
@@ -28,6 +56,10 @@ val Context.themeDataStore by preferencesDataStore(name = "theme_store")
 val Context.languageDataStore: DataStore<Preferences> by preferencesDataStore(name = "language_store")
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var mediaDownloadViewModel: MediaDownloadViewModel
+    private lateinit var gameViewModel: GameViewModel
+    private lateinit var menuMusicController: MenuMusicController
 
     override fun attachBaseContext(newBase: Context) {
         val language = runBlocking {
@@ -41,6 +73,54 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
 
+        val apiService = ApiService
+        val context = applicationContext
+        val db = AppDatabase.getDatabase(context)
+
+        val gameResultRepository = GameResultRepositoryImpl(db.gameResultDao())
+        val songRepository = SongRepositoryImpl(apiService, db)
+        val gameRepository = GameRepositoryImpl(apiService, db)
+        val filmRepository = FilmRepositoryImpl(apiService, db)
+        val fileProvider = AndroidFileProvider(context)
+        val daos = MediaDaos(
+            filmMediaDao = db.filmMediaDao(),
+            gameMediaDao = db.gameMediaDao(),
+            songAudioMediaDao = db.songAudioMediaDao(),
+            songVisualMediaDao = db.songVisualMediaDao()
+        )
+        val downloader = UnifiedMediaDownloader(
+            apiService,
+            daos = daos,
+            fileProvider.getAudioBaseDir(),
+            fileProvider.getVideoBaseDir()
+        )
+
+        val loadMediaUseCase = LoadMediaUseCase(
+            songRepository, gameRepository, filmRepository
+        )
+        val downloadMediaUseCase = DownloadMediaUseCase(
+            downloader
+        )
+        val setGameModeUseCase = SetGameModeUseCase()
+        val generateRoundsUseCase = GenerateRoundsUseCase()
+        val startGameUseCase = StartGameUseCase(
+            loadMediaUseCase, generateRoundsUseCase
+        )
+        val checkAnswerUseCase = CheckAnswerUseCase()
+        val nextRoundUseCase = NextRoundUseCase()
+        val resetGameUseCase = ResetGameUseCase()
+        val finishGameUseCase = FinishGameUseCase()
+        val insertResultUseCase = InsertResultUseCase(gameResultRepository)
+        val audioPlayer = ExoAudioPlayer(context, fileProvider.getAudioBaseDir())
+        val videoPlayer = ExoVideoPlayer(context, fileProvider.getVideoBaseDir())
+        val videoPreparer = VideoPreparer(videoPlayer)
+
+        mediaDownloadViewModel = MediaDownloadViewModel(
+            loadAudio = loadMediaUseCase, downloadMediaUseCase
+        )
+
+        val menuPlayer = MenuExoPlayer(this)
+
         lifecycleScope.launch {
 
             val theme = ThemeStorage(themeDataStore).getTheme()
@@ -53,25 +133,59 @@ class MainActivity : ComponentActivity() {
                 language
             )
 
+            menuMusicController = MenuMusicController(
+                scope = lifecycleScope,
+                themeFlow = settingsViewModel.theme,
+                player = menuPlayer
+            )
+
+            gameViewModel = GameViewModel(
+                setGameModeUseCase,
+                startGameUseCase,
+                checkAnswerUseCase,
+                nextRoundUseCase,
+                resetGameUseCase,
+                finishGameUseCase,
+                insertResultUseCase,
+                languageFlow = settingsViewModel.language,
+                audioPlayer,
+                videoPlayer,
+                videoPreparer
+            )
+
             enableFullScreenMode()
 
             setContent {
 
-//                val theme by settingsViewModel.theme.collectAsState()
-//                val language by settingsViewModel.language.collectAsState()
-//
-//                LaunchedEffect(language) {
-//                    AppLocaleController(this@MainActivity).apply(language)
-//                }
+                val theme by settingsViewModel.theme.collectAsState()
+                val language by settingsViewModel.language.collectAsState()
 
-//                SoundQuestThemeProvider(properties) {
-//                    //AppNavGraph()
-//                }
+                LaunchedEffect(language) {
+                    AppLocaleController(this@MainActivity).apply(language)
+                }
+
+                SoundQuestTheme(
+                    darkTheme = theme == AppTheme.DARK
+                ) {
+                    val navController = rememberNavController()
+                    AppNavHost(
+                        settingsViewModel = settingsViewModel,
+                        navController = navController,
+                        mediaDownloadViewModel = mediaDownloadViewModel,
+                        gameViewModel = gameViewModel,
+                        videoPlayer = videoPlayer,
+                        menuMusicController = menuMusicController
+                    )
+                }
             }
         }
     }
-}
 
+    override fun onDestroy() {
+        super.onDestroy()
+        menuMusicController.release()
+    }
+}
 
 fun ComponentActivity.enableImmersiveMode() {
     WindowInsetsControllerCompat(window, window.decorView).apply {
