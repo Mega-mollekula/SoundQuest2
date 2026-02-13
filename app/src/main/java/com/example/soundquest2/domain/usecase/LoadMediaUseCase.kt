7aefc13160db1811b49353863ee114f21af071b0
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-13 23:46:16 +0300

    refactor: rename of arguments

--- app/src/main/java/com/example/soundquest2/domain/usecase/LoadMediaUseCase.kt
+++ app/src/main/java/com/example/soundquest2/domain/usecase/LoadMediaUseCase.kt
@@ -10,8 +10,8 @@ import com.example.soundquest2.domain.util.MediaContentBalancer
 
 class LoadMediaUseCase(
     private val songRepository: SongRepository,
-    private val gameAudioRepository: GameRepository,
-    private val filmAudioRepository: FilmRepository
+    private val gameRepository: GameRepository,
+    private val filmRepository: FilmRepository
 ) {
     suspend operator fun invoke(forceRefresh: Boolean, gameMode: GameMode, language: String, count: Int = 10): Result<List<MediaContent>> {
         return when (gameMode) {
@@ -30,11 +30,11 @@ class LoadMediaUseCase(
             }
 
             is GameMode.GuessFilm -> {
-                filmAudioRepository.getRandomFilms(forceRefresh, count, language)
+                filmRepository.getRandomFilms(forceRefresh, count, language)
             }
 
             is GameMode.GuessGame -> {
-                gameAudioRepository.getRandomGames(forceRefresh, count, language)
+                gameRepository.getRandomGames(forceRefresh, count, language)
             }
 
             is GameMode.FastStart -> {
@@ -50,8 +50,8 @@ class LoadMediaUseCase(
     ): Result<List<MediaContent>> {
 
         val songsResult = songRepository.getRandomSongs(forceRefresh, language, count)
-        val filmsResult = filmAudioRepository.getRandomFilms(forceRefresh, count, language)
-        val gamesResult = gameAudioRepository.getRandomGames(forceRefresh, count, language)
+        val filmsResult = filmRepository.getRandomFilms(forceRefresh, count, language)
+        val gamesResult = gameRepository.getRandomGames(forceRefresh, count, language)
 
         val error = listOf(songsResult, filmsResult, gamesResult).filterIsInstance<Result.Error>().firstOrNull()?.error
 
