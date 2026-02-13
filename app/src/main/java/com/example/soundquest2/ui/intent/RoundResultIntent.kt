603cce645b20cb0348fe3d28f0ab918506b5f94a
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-13 23:57:39 +0300

    update: deleted intents

--- app/src/main/java/com/example/soundquest2/ui/intent/RoundResultIntent.kt
+++
@@ -1,7 +0,0 @@
-package com.example.soundquest2.ui.intent
-
-sealed interface RoundResultIntent {
-    data object ScreenShown : RoundResultIntent
-    data object ContinueClicked : RoundResultIntent
-    data object FavouriteClicked : RoundResultIntent
-}
--- app/src/main/java/com/example/soundquest2/ui/intent/SoundtracksSelectionIntent.kt
+++
@@ -1,7 +0,0 @@
-package com.example.soundquest2.ui.intent
-
-sealed interface SoundtracksSelectionIntent {
-    data object FilmsSelected : SoundtracksSelectionIntent
-    data object GamesSelected : SoundtracksSelectionIntent
-    data object BackClicked : SoundtracksSelectionIntent
-}
