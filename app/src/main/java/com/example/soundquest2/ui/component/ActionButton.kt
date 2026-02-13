9fb1ef86dd1337b46b48c91497aac064d193a6d7
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-13 23:52:49 +0300

    refactor: deleted preview

--- app/src/main/java/com/example/soundquest2/ui/component/ActionButton.kt
+++ app/src/main/java/com/example/soundquest2/ui/component/ActionButton.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -14,12 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.soundquest2.R
-import com.example.soundquest2.ui.theme.AppTheme
 
 @Composable
 fun ActionButton(
@@ -48,23 +41,5 @@ fun ActionButton(
             color = MaterialTheme.colorScheme.onPrimary,
             style = MaterialTheme.typography.labelSmall
         )
-    }
-}
-
-@Preview(
-    showBackground = true,
-    locale = "ru"
-)
-@Composable
-fun BackButtonPreview() {
-    AppTheme(false) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black)
-                .padding(16.dp)
-        ) {
-            ActionButton(stringResource(R.string.exit)) {}
-        }
     }
 }
