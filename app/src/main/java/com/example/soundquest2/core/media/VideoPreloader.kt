cc2284f64a34f71d94a52dcd42649dbc283f68b7
Mega-mollekula <mega_mollekula2020@mail.ru>
2026-02-17 15:54:50 +0300

    update: deleted files

--- app/src/main/java/com/example/soundquest2/core/media/VideoPreloader.kt
+++
@@ -1,9 +0,0 @@
-package com.example.soundquest2.core.media
-
-class VideoPreloader(
-    private val videoPlayer: VideoPlayer
-) {
-    fun preloadVideos(paths: List<String>) {
-        videoPlayer.prepare(paths)
-    }
-}
